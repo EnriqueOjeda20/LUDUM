d27dc759ac8d510a21b59855519bf7ca0db0fd0b
VictorGouro <89236143+VictorGouro@users.noreply.github.com>
2021-09-14 00:16:07 -0500

    Creacion TipoPago

--- src/main/java/pe/edu/upc/entities/Reputacion.java
+++ src/main/java/pe/edu/upc/entities/Reputacion.java
@@ -1,4 +1,5 @@
 package pe.edu.upc.entities;
+
 import javax.persistence.Column;
 
 import javax.persistence.Entity;
@@ -8,54 +9,61 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Reputacion")
+@Table(name = "Reputacion")
 
 public class Reputacion {
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY) 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigoReputacion;
-	@Column(name= "numeroEquipo" , nullable = false, length = 45) /// asdasd
-	private int numeroEquipo; //aca seria el llamar a la tabla de usuario 
-	@Column (name= "puntos" , nullable = false, length = 8)
+	@Column(name = "numeroEquipo", nullable = false, length = 45) /// asdasd
+	private int numeroEquipo; // aca seria el llamar a la tabla de usuario
+	@Column(name = "puntos", nullable = false, length = 8)
 	private int puntos;
-	@Column (name= "estado" , nullable = false, length = 20)
+	@Column(name = "estado", nullable = false, length = 20)
 	private boolean estado;
-	
-	public Reputacion()
-	{
+
+	public Reputacion() {
 		super();
 	}
-	public Reputacion(int codigoReputacion, int numeroEquipo, int puntos, boolean estado)
-	{
+
+	public Reputacion(int codigoReputacion, int numeroEquipo, int puntos, boolean estado) {
 		super();
-		this.codigoReputacion= codigoReputacion;
-		this.numeroEquipo= numeroEquipo;
-		this.puntos= puntos;
-		this.estado= estado;
+		this.codigoReputacion = codigoReputacion;
+		this.numeroEquipo = numeroEquipo;
+		this.puntos = puntos;
+		this.estado = estado;
 	}
+
 	public int getCodigoReputacion() {
 		return codigoReputacion;
 	}
+
 	public void setCodigoReputacion(int codigoReputacion) {
 		this.codigoReputacion = codigoReputacion;
 	}
+
 	public int getNumeroEquipo() {
 		return numeroEquipo;
 	}
+
 	public void setNumeroEquipo(int numeroEquipo) {
 		this.numeroEquipo = numeroEquipo;
 	}
+
 	public int getPuntos() {
 		return puntos;
 	}
+
 	public void setPuntos(int puntos) {
 		this.puntos = puntos;
 	}
+
 	public boolean getEstado() {
 		return estado;
 	}
+
 	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
-	
+
 }
