1ff3213cba7ab621ac7362fb037b12fd0a8def40
Scarion22 <70586739+Scarion22@users.noreply.github.com>
2021-09-14 23:48:38 -0500

    Tabla usuario Eliminacion tabla reputacion

--- src/main/java/pe/edu/upc/entities/Usuario.java
+++ src/main/java/pe/edu/upc/entities/Usuario.java
@@ -1,5 +1,79 @@
 package pe.edu.upc.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Usuario")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int codigoUsuario;
+	@Column(name= "nombreUsuario" , nullable = false, length = 45)
+	private String nombreUsuario;
+	@Column (name= "correo" , nullable = false, length = 8)
+	private String correo;
+	@Column (name= "apellido" , nullable = false, length = 20)
+	private String apellido;
+	@Column (name= "password" , nullable = false, length = 20)
+	private String password;
+	@Column (name= "nickname" , nullable = false, length = 20)
+	private String nickname;
+	public Usuario()
+	{
+		super();
+	}
+	public Usuario(int codigoUsuario, String nombreUsuario, String correo, String apellido, String password, String nickname)
+	{
+		super();
+		this.codigoUsuario= codigoUsuario;
+		this.nombreUsuario= nombreUsuario;
+		this.correo=correo;
+		this.apellido=apellido;
+		this.password= password;
+		this.nickname= nickname;
+		
+    }
+	public int getCodigoUsuario() {
+		return codigoUsuario;
+	}
+	public void setCodigoUsuario(int codigoUsuario) {
+		this.codigoUsuario = codigoUsuario;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
 
 }
