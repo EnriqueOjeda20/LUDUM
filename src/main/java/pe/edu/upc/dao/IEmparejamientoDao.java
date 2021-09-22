661c71d2e2f8ae103a6856715a3bfb6423e4a98a
VictorGouro <89236143+VictorGouro@users.noreply.github.com>
2021-09-22 16:45:16 -0500

    Creacion de las tablas service, dao, dao implements de UsuarioVideoJUEGO

--- src/main/java/pe/edu/upc/dao/IEmparejamientoDao.java
+++ src/main/java/pe/edu/upc/dao/IEmparejamientoDao.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import pe.edu.upc.entities.Emparejamiento;
 
-public interface IEmparejamientoDao {
+public interface IEmparejamientoDao 
+{
 	public void insert(Emparejamiento ec);
 
 	public List<Emparejamiento> list();
