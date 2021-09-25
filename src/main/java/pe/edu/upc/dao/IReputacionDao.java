186cb04071888c7f2b49baa11bc46f3166e74d70
Scarion22 <70586739+VictorBenitesReyna@users.noreply.github.com>
2021-09-25 01:01:44 -0500

    eliminar-modificar : Reputacion

--- src/main/java/pe/edu/upc/dao/IReputacionDao.java
+++ src/main/java/pe/edu/upc/dao/IReputacionDao.java
@@ -6,4 +6,6 @@ public interface IReputacionDao {
 	public void insert(Reputacion rc);
 	public List<Reputacion> list();
 	public void Eliminar(int codigoReputacion);
+	public List<Reputacion> finByReputacion(Reputacion rcc);
+	public void modificar(Reputacion rc);
 }
--- src/main/java/pe/edu/upc/service/IReputacionService.java
+++ src/main/java/pe/edu/upc/service/IReputacionService.java
@@ -5,9 +5,10 @@ import java.util.List;
 import pe.edu.upc.entities.Reputacion;
 
 public interface IReputacionService {
-	public void insert(Reputacion r);
-	public List <Reputacion> list();
-	
+	public void insert(Reputacion rc);
+	public List<Reputacion> list();
 	public void Eliminar(int codigoReputacion);
+	public List<Reputacion> finByReputacion(Reputacion rcc);
+	public void modificar(Reputacion rc);
 
 }
